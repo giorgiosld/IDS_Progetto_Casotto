d60622ac8cf89c56b24e41f336d493a7d696a23a
girogiosld <giorgiosaldana99@gmail.com>
2021-12-30 10:53:26 +0100

    Initial commit

---
+++ src/main/java/it/unicam/cs/ids_progetto_casotto/IdsProgettoCasottoApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids_progetto_casotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdsProgettoCasottoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdsProgettoCasottoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unicam/cs/ids_progetto_casotto/IdsProgettoCasottoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids_progetto_casotto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdsProgettoCasottoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
