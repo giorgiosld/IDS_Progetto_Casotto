0bf81e0ec67cbd1f2e3daa146739eeec17d72ca7
girogiosld <giorgiosaldana99@gmail.com>
2022-02-18 16:50:48 +0100

    Inizio implementazione spring

--- src/main/java/it/unicam/cs/ids_progetto_casotto/model/ordinazione/Bibita.java
+++ src/main/java/it/unicam/cs/ids_progetto_casotto/model/ordinazione/Bibita.java
@@ -1,16 +1,24 @@
 package it.unicam.cs.ids_progetto_casotto.model.ordinazione;
 
+import javax.persistence.Entity;
+
 /**
  * Classe che rappresenta un generica bibita che si trova nel men&ugrave;
  */
+@Entity
 public class Bibita extends Consumazione{
 
     private boolean aperitivo;
 
-    public Bibita(String nome, double prezzo,  boolean aperitivo) {
+    /*public Bibita(String nome, double prezzo,  boolean aperitivo) {
         super(nome, prezzo);
         this.aperitivo = aperitivo;
+    }*/
+
+    public Bibita() {
+        super();
     }
+
 
     /**
      * Controlla se si richiede l'aperitivo
