640f4a00acd7bb24720c54d77da4bc72f8ffbb19
lollobeach <verduccii46@gmail.com>
2022-02-22 23:43:08 +0100

    Risoluzione problemi String

--- src/main/java/it/unicam/cs/ids_progetto_casotto/controller/controller_utenza/PrenotazioneUtenzaCliente.java
+++ src/main/java/it/unicam/cs/ids_progetto_casotto/controller/controller_utenza/PrenotazioneUtenzaCliente.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
  * del cliente che viene gestita da
  * {@link ControllerPeriodo} e {@link it.unicam.cs.ids_progetto_casotto.model.Receptionist}
  */
+
 public class PrenotazioneUtenzaCliente {
 
     private final int idCliente;
